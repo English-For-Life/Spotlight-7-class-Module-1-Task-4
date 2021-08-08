4bddc94c00c698084c691bbe44e6c4d8e3cdbc66
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-08-08 22:36:55 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,66 @@
+//Array of objects
+const quiz = [
+
+    {
+        q:"...... he ...... his homework now?",
+        options:["Is / doing", "Does / do"],
+        answer: 0
+    },
+    {
+        q:"What time ...... you ...... work?",
+        options:["are / starting", "do / start"],
+        answer: 1
+    },
+    {
+        q:"He ...... TV now.",
+        options:["isn't watching", "doesn't watch"],
+        answer: 0
+    },
+    {
+        q:"She ...... flying by plane.",
+        options:["isn't like", "doesn't like"],
+        answer: 1
+    },
+    {
+        q:"...... they ...... next week?",
+        options:["Are / leaving", "Do / leave"],
+        answer: 0
+    },
+    {
+        q:"I ...... he's in.",
+        options:["am not thinking", "don't think"],
+        answer: 1
+    },
+    {
+        q:"John ...... his parents this weekend.",
+        options:["is visiting", "visits"],
+        answer: 0
+    },
+    {
+        q:"He ...... me to school every day.",
+        options:["is taking", "takes"],
+        answer: 1
+    },
+    {
+        q:"Be quiet. The baby ......  .",
+        options:["is sleeping", "sleeps"],
+        answer: 0
+    },
+    {
+        q:"He always ...... his nails.",
+        options:["is biting", "bites"],
+        answer: 1
+    },
+    {
+        q:"Water ...... at 80 C .",
+        options:["doesn't boil", "isn't boiling"],
+        answer: 0
+    },
+    {
+        q:"The bus ...... at 6.30 PM.",
+        options:["is leaving", "leaves"],
+        answer: 1
+    }
+
+
+]
